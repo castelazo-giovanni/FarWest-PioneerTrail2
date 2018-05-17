9f7edf037f4b41e85649e44206e6823b9437a215
castelazo-giovanni <38674339+castelazo-giovanni@users.noreply.github.com>
2018-05-16 20:52:23 -0500

    Tested FarWest and Player functions

---
+++ FarWestGame/src/byui/cit260/farWestGame/model/FarWestGame.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.farWestGame.model;
+
+/**
+ *
+ * @author Giovanni
+ */
+public class FarWestGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Player player1 = new Player();
+        player1.setName("Gio");
+        System.out.println(player1.toString());
+        
+        
+        
+        
+    }
+    
+}
