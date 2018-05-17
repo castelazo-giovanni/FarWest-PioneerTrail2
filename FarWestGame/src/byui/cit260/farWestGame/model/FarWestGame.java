3fc785bdba9dcff3d6cf4be15f38710374a103df
castelazo-giovanni <38674339+castelazo-giovanni@users.noreply.github.com>
2018-05-16 21:35:17 -0500

    Tested game, supply, and player classes for Team Assignment

--- FarWestGame/src/byui/cit260/farWestGame/model/FarWestGame.java
+++ FarWestGame/src/byui/cit260/farWestGame/model/FarWestGame.java
@@ -15,10 +15,19 @@ public class FarWestGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //game class Giovanni Castelazo
+        Game game1 = new Game();
+        game1.setGameName("Far West");
+        System.out.println(game1.toString());
+        
+        //player class Agustin Gimenez
         Player player1 = new Player();
         player1.setName("Gio");
         System.out.println(player1.toString());
-        
+                
+        //supply class Araceli Camarillo
+        Supply supply1 = new Supply("hammer", 1, 5);
+        System.out.println(supply1.toString());
         
         
         
