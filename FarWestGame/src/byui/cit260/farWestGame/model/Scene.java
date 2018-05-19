aa44ef53ee5814af3928ff6fffb2672dd49a89e5
castelazo-giovanni <38674339+castelazo-giovanni@users.noreply.github.com>
2018-05-19 14:23:35 -0500

    Changed illness in scene class from integer to string

--- FarWestGame/src/byui/cit260/farWestGame/model/Scene.java
+++ FarWestGame/src/byui/cit260/farWestGame/model/Scene.java
@@ -10,37 +10,37 @@ package byui.cit260.farWestGame.model;
  * @author Giovanni
  */
 public enum Scene {
-    Navoo("Nauvoo", "Description of Navoo TBD", 1), 
-    SugarCreek("Sugar Creek", "Description of Sugar Creek TBD", 0),
-    RichardsonsPoint("Richardson's Point", "Description of Richardson's Point TBD", 0),
-    CharitonRiverCrossing("Chariton River Crossing", "Description of CRC TBD", 3),
-    LocustCreek("Locust Creek", "Description of LC TBD", 2),
-    GardenGrove("GardenGrove", "Description of Garden Grove TBD", 0),
-    NishnabotnaRiver("Nishnabotna River", "Description TBD", 0),
-    GrandEncampment("Grand Encampment", "Description TBD", 0),
-    CouncilBluffs("Council Bluffs", "Description TBD", 0),
-    WinterQuarters("Winter Quarters", "Description TBD", 0),
-    ElkhornRiverCrossing("Elkhorn River Crossing", "Description TBD", 0),
-    PlatteRiver("PlatteRiver", "Description TBD", 0),
-    FortKearny("Fort Kearny", "Description TBD", 0),
-    ConfluencePoint("Confluence Point", "Description TBD", 0),
-    AshHollow("Ash Hollo", "Description TBD", 0),
-    ChimneyRock("Chimney Rock", "Description TBD", 0),
-    ScottsBluff("Scott's Bluff", "Description TBD", 0),
-    FortLaramie("Fort Laramie", "Description TBD", 0),
-    SweetwaterRiver("Sweetwater River", "Description TBD", 0),
-    IndependenceRock("Independence Rock", "Description TBD", 0),
-    FortBridger("Fort Bridger", "Description TBD", 0),
-    EchoCanyon("Echo Canyon", "Description TBD", 0),
-    GoldenPassRoad("Golden Pass Road", "Description TBD", 0),
-    EmigrationCanyon("Emigration Canyon", "Description TBD", 0),
-    Zion("Zion", "Description TBD", 0);
+    Navoo("Nauvoo", "Description of Navoo TBD", "Fever"), 
+    SugarCreek("Sugar Creek", "Description of Sugar Creek TBD", "Hypothermia"),
+    RichardsonsPoint("Richardson's Point", "Description of Richardson's Point TBD", "Snake Bite"),
+    CharitonRiverCrossing("Chariton River Crossing", "Description of CRC TBD", "Broken Bone"),
+    LocustCreek("Locust Creek", "Description of LC TBD", "None"),
+    GardenGrove("GardenGrove", "Description of Garden Grove TBD", "None"),
+    NishnabotnaRiver("Nishnabotna River", "Description TBD", "Snake Bite"),
+    GrandEncampment("Grand Encampment", "Description TBD", "Hypothermia"),
+    CouncilBluffs("Council Bluffs", "Description TBD", "Fever"),
+    WinterQuarters("Winter Quarters", "Description TBD", "None"),
+    ElkhornRiverCrossing("Elkhorn River Crossing", "Description TBD", "None"),
+    PlatteRiver("PlatteRiver", "Description TBD", "Hypothermia"),
+    FortKearny("Fort Kearny", "Description TBD", "None"),
+    ConfluencePoint("Confluence Point", "Description TBD", "Broken Bone"),
+    AshHollow("Ash Hollo", "Description TBD", "None"),
+    ChimneyRock("Chimney Rock", "Description TBD", "None"),
+    ScottsBluff("Scott's Bluff", "Description TBD", "None"),
+    FortLaramie("Fort Laramie", "Description TBD", "Snake Bite"),
+    SweetwaterRiver("Sweetwater River", "Description TBD", "None"),
+    IndependenceRock("Independence Rock", "Description TBD", "Broken Bone"),
+    FortBridger("Fort Bridger", "Description TBD", "None"),
+    EchoCanyon("Echo Canyon", "Description TBD", "Broken Bone"),
+    GoldenPassRoad("Golden Pass Road", "Description TBD", "None"),
+    EmigrationCanyon("Emigration Canyon", "Description TBD", "None"),
+    Zion("Zion", "Description TBD", "None");
 
     private String sceneName;
     private String sceneDescription;
-    private Integer sceneIllness;
+    private String sceneIllness;
 
-    Scene(String sceneName, String sceneDescription, Integer sceneIllness){
+    Scene(String sceneName, String sceneDescription, String sceneIllness){
         this.sceneName = sceneName;
         this.sceneDescription = sceneDescription;
         this.sceneIllness = sceneIllness;
@@ -54,7 +54,7 @@ public enum Scene {
         return sceneDescription;
     }
 
-    public Integer getSceneIllness() {
+    public String getSceneIllness() {
         return sceneIllness;
     }
 
