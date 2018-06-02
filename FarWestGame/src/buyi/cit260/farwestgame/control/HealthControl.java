b9e20e0bec6611f2187b6881076ece6f7e3294a9
FGL <FGL@DESKTOP-L1JHTSK>
2018-06-02 19:58:51 -0300

    Created HealthControl class to start working on individual assignment.

---
+++ FarWestGame/src/buyi/cit260/farwestgame/control/HealthControl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buyi.cit260.farwestgame.control;
+
+/**
+ *
+ * @author Agustin FGL (Individual Assignment)
+ */
+public class HealthControl {
+    
+}
