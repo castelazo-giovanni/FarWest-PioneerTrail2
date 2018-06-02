38632ce7e57d41bc53a5ef5a7539a7dd29cd3af8
Camarillo-Araceli <aracelicaes@users.noreply.github.com>
2018-06-01 21:50:47 -0500

    adding calSupplyUsed function 3

--- FarWestGame/src/buyi/cit260/farwestgame/control/ResourceControl.java
+++ FarWestGame/src/buyi/cit260/farwestgame/control/ResourceControl.java
@@ -113,8 +113,15 @@ public class ResourceControl {
         beginningWood =- woodUsed;
         
         double supplyWeight = (beginningWheel * 50) + (beginningBullets * 0.1) + (beginningWood * 10);
+        
+        double[] supplyResult;
+        supplyResult = new double [4]; //create an array of 4 elements
+        supplyResult[0] = beginningWheel;
+        supplyResult[1] = beginningBullets;
+        supplyResult[2] = beginningWood;
+        supplyResult[3] = supplyWeight;
 
-        return int [beginningWheel, beginningBullets, beginningWood, supplyWeight];
+        return (supplyResult);
 
     }
 
