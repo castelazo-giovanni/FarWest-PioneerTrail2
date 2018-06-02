bf556480cff650223d8879b55cd50c908f651563
FGL <FGL@DESKTOP-L1JHTSK>
2018-06-02 20:31:39 -0300

    Created gameWonLost function under HealthControl class.

--- FarWestGame/src/buyi/cit260/farwestgame/control/HealthControl.java
+++ FarWestGame/src/buyi/cit260/farwestgame/control/HealthControl.java
@@ -5,10 +5,25 @@
  */
 package buyi.cit260.farwestgame.control;
 
+import byui.cit260.farWestGame.model.Actor;
+import java.util.List;
+
 /**
  *
  * @author Agustin FGL (Individual Assignment)
  */
 public class HealthControl {
-    
+    public boolean gameWonLost(List<Actor> family){
+       int count=0;
+       for(Actor actor: family){
+           if(actor.isAlive()){
+               count++;
+           }
+       }
+       if(count>2){
+           return true;
+       }else{
+           return false;
+       }
+   }
 }
