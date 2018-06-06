b71cc67fcae4cded8f36b96b81b14d6b0cb1b6fe
castelazo-giovanni <38674339+castelazo-giovanni@users.noreply.github.com>
2018-06-06 16:25:50 -0500

    Lesson 7 Team assignment up to page 8 step 5

---
+++ FarWestGame/src/byui/cit260/farWestGame/view/StartProgramView.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.farWestGame.view;
+
+/**
+ *
+ * @author Giovanni
+ */
+public class StartProgramView {
+
+    public StartProgramView() {
+        
+    }
+        
+    private void displayStartProgramView() {
+            boolean endOfView;
+            endOfView = false;
+            do {
+              String[] inputs = this.geInputs();
+              if (inputs.length < 1 || inputs[0] == "Q"){
+                  continue;
+              }   
+              endOfView = doAction(inputs); 
+            } while (endOfView != true);
+
+    }
+
+    private boolean doAction(String[] inputs) {
+        System.out.println("**** doAction() called ***");
+        System.out.println("\tinputs = " + inputs[0]);
+        
+        return true;
+    }
+
+    private String[] geInputs() {
+        System.out.println("**** getInputs() called ****");
+        
+        String[] inputs = new String[1];
+        inputs[0]= "testInput";
+
+        return inputs;
+    }
+    
+   
+}
+
