ff9a7cdbf259cf51d0ecd622620cdc7923af3bb6
Camarillo-Araceli <aracelicaes@users.noreply.github.com>
2018-06-13 23:04:37 -0500

    added dtls into resources menu view class

--- FarWestGame/src/byui/cit260/farWestGame/view/ResourcesMenuView.java
+++ FarWestGame/src/byui/cit260/farWestGame/view/ResourcesMenuView.java
@@ -5,6 +5,10 @@
  */
 package byui.cit260.farWestGame.view;
 
+import byui.cit260.farWestGame.control.GameControl;
+import farwestgame.FarWestGame;
+import java.util.Scanner;
+
 /**
  *
  * @author kitka_000
@@ -16,7 +20,59 @@ public class ResourcesMenuView {
     
     public void displayResourcesMenuView() {
         boolean endOfView = false;
+        do {
+            String[] inputs = this.getInputs();
+            if (inputs.length < 1 || inputs[0].toUpperCase().equals("Q")) {
+                endOfView = true;
+                continue;
+            }
+            endOfView = doAction(inputs);
+
+        } while (endOfView != true);
     
     }
     
+    public String[] getInputs() {
+        String[] inputs = new String[1];
+        System.out.println("F - Insert How Much Food\n"
+                + "R - Restart existing game\n"
+                + "H - Get help on how to play the game\n"
+                + "E - Exit");
+        // Display a description of the view
+        boolean valid = false;
+        while (valid == false) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Select option");
+            inputs[0] = scanner.nextLine();
+            inputs[0] = inputs[0].trim();
+            if (inputs[0].length() < 1) {
+                System.out.println("You must enter a non-blank value");
+            }
+            valid = true;
+
+        }
+        return inputs;
+    }
+    
+    private boolean doAction(String[] inputs) {
+        String menuItem = inputs[0].toUpperCase();
+        switch (menuItem) {
+            case "N":
+                startNewGame();
+                break;
+            case "R":
+                restartGame();
+                break;
+            case "H":
+                getHelp();
+                break;
+            case "E":
+                return true;
+            default:
+                System.out.println("Invalid menu item.");
+        }
+        return false;
+    }
+
+  
 }
